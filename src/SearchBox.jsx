73c1f2dc343aa124b26d76b0a1aa38495f098569
Pankaj <shubhu2004sharma@gmail.com>
2026-08-09 14:04:08 +0530

    feat: integrate OpenWeather API and fetch weather data

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,15 +5,33 @@ import { useState } from "react";
 
 export default function SearchBox() {
   let [city, setCity] = useState("");
+  const API_URL =
+    "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "3823fff3d053aeba8b6f48d8f1b22928";
+
+  let getWeatherInfo = async () => {
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse = await response.json();
+    let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_like,
+        weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
 
   let handleChange = (event) => {
     setCity(event.target.value);
-  }; 
+  };
 
   let handleSubmit = (event) => {
     event.preventDefault();
     console.log(city);
     setCity("");
+    getWeatherInfo();
   };
 
   return (
